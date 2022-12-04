26916eb383670871b6e4f578f6333b55d164ad00
ped-aguiar98 <58562868+ped-aguiar98@users.noreply.github.com>
2022-12-04 05:02:39 -0300

    Fix: confirmation massage from modal

--- src/components/ImageCardDetail.js
+++ src/components/ImageCardDetail.js
@@ -20,7 +20,7 @@ const ImageCardDetail = () => {
       {choice &&
       <div className="flex justify-end">
         <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"  role="alert">
-          <strong class="font-bold pr-4">Investimento confirmado</strong>
+          <strong class="font-bold pr-4">Parabéns, agora você é sócio desta empresa!</strong>
           <span class="block sm:inline"></span>
           <span class="absolute top-0 bottom-0 right-0 pl-40 py-3">
             <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
@@ -57,19 +57,6 @@ const ImageCardDetail = () => {
       <div class='relative h-full min-h-fit gap-4 max-w-7x1 p-4 m-auto z-10 rounded-b-xl text-white bg-cyan-600 bg-opacity-90'>
         <CardBottom/>
       </div>
-
-      {/* {choice &&
-      <div className="flex justify-center">
-        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-          <strong class="font-bold">Investimento confirmado</strong>
-          <span class="block sm:inline"></span>
-          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-            <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-          </span>
-        </div>
-      </div>
-      }
-      {modalOn && < Modal setModalOn={setModalOn} setChoice={setChoice} />} */}
     </div>
   )
 }
