2387967c2c72599de524d1afa94197ca462452e6
ped-aguiar98 <58562868+ped-aguiar98@users.noreply.github.com>
2022-12-04 04:07:12 -0300

    Fix: components ImageCardDetail and Modal

--- src/components/ImageCardDetail.js
+++ src/components/ImageCardDetail.js
@@ -16,7 +16,19 @@ const ImageCardDetail = () => {
   }
 
   return (
-    <div class='relative w-full h-auto py-4 px-7'>
+    <div class='relative w-full h-auto  px-7'>
+      {choice &&
+      <div className="flex justify-end">
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"  role="alert">
+          <strong class="font-bold pr-4">Investimento confirmado</strong>
+          <span class="block sm:inline"></span>
+          <span class="absolute top-0 bottom-0 right-0 pl-40 py-3">
+            <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+          </span>
+        </div>
+      </div>
+      }
+      {modalOn && < Modal setModalOn={setModalOn} setChoice={setChoice} />}
       <div class='relative h-full min-h-fit grid grid-cols-2 gap-9 p-4 max-w-7x1 m-auto z-10 rounded-t-xl bg-zinc-300 bg-opacity-50'>
         <div class='relative h-128'>
           <img src={image} alt="" className=" object-fill rounded-2xl px-1"/>
@@ -27,7 +39,7 @@ const ImageCardDetail = () => {
               <h1 class=' text-start text-2xl font-bold '>Empresa Ficticia</h1>
             </div>
             <div class='justify-self-end px-9'>
-              <button class="mx-2 my-1 md:my-0 px-6 py-2 font-semibold text-xl rounded text-white bg-amber-500 hover:bg-amber-600 duration-500 inline-block" onClick={clicked}>
+              <button class="mx-2 my-1 md:my-0 px-20 py-2 font-semibold text-xl rounded text-white bg-amber-500 hover:bg-amber-600 duration-500 inline-block" onClick={clicked}>
                 Investir
               </button>
             </div>
@@ -46,18 +58,18 @@ const ImageCardDetail = () => {
         <CardBottom/>
       </div>
 
-      {choice &&
+      {/* {choice &&
       <div className="flex justify-center">
         <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-  <strong class="font-bold">Investimento confirmado</strong>
-  <span class="block sm:inline"></span>
-  <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-    <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-  </span>
-</div>
+          <strong class="font-bold">Investimento confirmado</strong>
+          <span class="block sm:inline"></span>
+          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+          </span>
         </div>
+      </div>
       }
-      {modalOn && < Modal setModalOn={setModalOn} setChoice={setChoice} />}
+      {modalOn && < Modal setModalOn={setModalOn} setChoice={setChoice} />} */}
     </div>
   )
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ImageCard from './ImageCard'
 
 const Modal = ( {setModalOn, setChoice} ) => {
 
@@ -13,25 +14,55 @@ const handleCancelClick = () => {
 
   return (
     <div className="   bg-zinc-200 fixed inset-0 z-50   ">
-  
-              <div className="flex h-screen justify-center items-center opacity 100">
-  
-                  <div className="flex-col justify-center  bg-white py-12 px-24 border-4 rounded-xl opacity-100">
-
-                      <div className="flex  text-lg font-bold text-zinc-600   mb-10" >Confirmação de Investimento</div>
-                      <div>
-                        <div class=''>
-                        
-                        </div>
-                        <div class='grid grid-cols-2 gap-2'>
-                          <button onClick={handleOKClick} className=" rounded px-4 py-2 text-white  bg-cyan-500 ">Confirmar</button>
-                          <button onClick={handleCancelClick} className="rounded px-4 py-2 ml-4 text-white bg-black ">Cancelar</button>
-                        </div>
-                      </div>
-
-                  </div>
-              </div>
+      <div className="flex h-screen justify-center items-center opacity 100">
+        <div className="flex-col justify-center bg-white py-12 px-24 border-4 rounded-xl opacity-100 pl-40">
+         <div className="flex text-3xl tex font-bold text-cyan-700  mb-4" >
+          <h1 class>Confirmação de Investimento</h1>
           </div>
+           <div>
+             <div>
+              <form class="bg-white rounded px-0 pt-6 pb-8 mb-4 grid grid-cols-2 gap-7">
+                <div class="mb-4">
+                  <h1 class="text-gray-700 text-lg text-left font-bold mb-2 flex flex-auto" for="username">
+                    <p class='text-cyan-500'>[User]</p>, pronto para fazer parte dessa jornada com a <p class='text-cyan-500'> [Empresa]</p>?
+                  </h1>
+                  <p class='text-left pt-16 pb-6'>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                  </p>
+                  <input class="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="number" placeholder="R$"></input>
+                  <div className="form-control">
+                  <div class='pt-7'>
+                    <label className="label cursor-pointer">
+                      <div class='text-left'>
+                      <span className="label-text text- pr-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                      <div class='pt-2'>
+                        <span class='font-bold pr-2'>Concordo com os termos</span>
+                        <input type="checkbox" checked className="checkbox checkbox-primary" />
+                      </div>
+                      </span> 
+                      
+                      
+                      </div>
+                    </label>
+                    </div>
+                  </div>
+                </div>
+                <div class='w-fit'>
+                  <ImageCard/>
+                </div>
+                <div class="flex items-center justify-between">
+                </div>
+              </form>
+          </div>
+          <div class='grid grid-cols-12 gap-2'>
+            <button onClick={handleOKClick} className=" rounded px-4 py-2 text-white  bg-cyan-500 col-start-1 col-end-6 ">Confirmar</button>
+            <button onClick={handleCancelClick} className="rounded px-4 py-2 ml-4  text-white bg-black col-start-6 col-end-11">Cancelar</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    </div>
   )
 }
 
